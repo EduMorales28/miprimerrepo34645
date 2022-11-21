4ab506322324357df15657fb3a447b7379286a10
EduMorales28 <92487964+EduMorales28@users.noreply.github.com>
2022-11-21 23:56:02 +0000

    suma

---
+++ clase16.py
@@ -0,0 +1,7 @@
+def funcionsuma():
+    numero1 = int(input("Ingrese el primer numero: "))
+    numero2 = int(input("Ingrese el segundo numero: "))
+
+    suma = numero1 + numero2
+    print(f"La suma es {suma}")
+funcionsuma()
